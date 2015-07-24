d8aabcc59d78d18b635aa1eb0eefa1dfb1deafd2
Nicholas Curtis <nich.curtis@gmail.com>
2015-07-23 22:41:35 -0700

    Update aliases.zsh

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -7,15 +7,3 @@ alias lesscolor='less -R'
 alias morecolor='more -R'
 
 alias lsa='ls -lah'
-
-alias vudo=vgdo
-
-alias mblt='lt --host "http://tunnel.matchbooklab.com"'
-alias cwst='lt --host "http://tunnel.cameronspear.com"'
-
-# fasd shortcuts
-alias j="fasd_cd -d"
-alias ji="fasd_cd -i"
-
-# unalias things from prezto that conflict!
-unalias lt # local tunnel
